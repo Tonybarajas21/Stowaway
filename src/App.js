eedc4a0edcb0ed4f1e5bfafb4ed9a3d245fbfea2
TonyBee <josebarajas2169@gmail.com>
2019-04-22 21:11:11 -0600

    appjs working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from './utils/main';
 import LandingPage from './landingPage';
-import RegisterBoats from './registerBoats';
+// import UserProfile from "./components/profile";
+// import { StripeProvider } from "react-stripe-elements";
+// import Checkout from "./components/Checkout.js"
+import Login from './components/Login';
+// import SignUp from "./components/signUp"
+
+// import { ReactiveBase, DataSearch, DateRange, RangeSlider, ResultCard } from '@appbaseio/reactivesearch';
+import Register from './components/Register';
+// import './App.css';
 
 
-const App = () => (
+function App () {
+    return (
     <Router>
         <div>
             <Switch>
-                <Route exact path ="/" component={LandingPage} />
+                <Route exact path="/" component={LandingPage} />
                 <Route exact path="/Main" component={Main} />
-                <Route exact path="/Boats" component={RegisterBoats} />
+                
+                <Route exact path="/Login" component={Login} />
+            
+                
+                <Route exact path="/Register" component={Register} />
+
             </Switch>
-        </div>
-    </Router>
-)
+         </div>
+     </Router>
+    
+    );
+
+}
 
 export default App;
